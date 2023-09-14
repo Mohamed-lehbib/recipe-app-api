bd881089773373075c779c4877bd415a62a6d70a
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-09-14 15:32:48 +0000

    fixing the linting error in test

--- app/core/models.py
+++ app/core/models.py
@@ -8,20 +8,22 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
-    def create_user(self, email, password=None, **extra_field):
+
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError('User most have an email adress.')
-        user = self.model(email=self.normalize_email(email), **extra_field)
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
-        """create and return new superuser."""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -29,8 +31,9 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    """user in the system."""
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
