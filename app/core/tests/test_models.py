40ec4e979172c9f45cb7211181361b03e47cf873
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-09-14 15:18:41 +0000

    fixing the linting error in test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successfull."""
         email = 'test@example.com'
